bf52268531658ba0cbd9af8e9fd2e6cb169da306
Halimatou cisse <halimatou.cisse@gmail.com>
2022-06-28 20:47:18 +0200

    mise a jour backend user ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
 // console.log(process.env.JWT_KEY_TOKEN);
 
@@ -10,12 +10,12 @@ exports.signup = (req, res, next) => {
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
+        username: req.body.username,
         email: req.body.email,
         password: hash, // le mot de passe haché est enregistré dans la bdd
         isAdmin: false,
       });
+
       user
         .save()
         .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
@@ -23,3 +23,49 @@ exports.signup = (req, res, next) => {
     })
     .catch((error) => res.status(500).json({ error }));
 };
+
+// pour la connexion
+
+exports.signin = (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: "Utilisateur non trouvé." });
+      return;
+    }
+    if (!user) {
+      return res.status(404).send({ message: "Utilisateur non trouvé." });
+    }
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Mot de passe incorrect!" });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_KEY_TOKEN, {
+      expiresIn: 86400, // 24 hours
+    });
+
+    res.status(200).send({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: token,
+    });
+  });
+};
+
+//pour la deconnexion (efface la session)
+exports.signout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.status(200).send({ message: "Vous êtes déconnecté!" });
+  } catch (err) {
+    this.next(err);
+  }
+};
+
+// faire delete
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+//les routes pour l'authentification (connexion inscription deconnexion)
 const userCtrl = require("../controllers/user");
-const password = require("../middleware/password");
-const auth = require("../middleware/auth");
+const password = require("../middleware/password"); // on vérifie la validité du mdp
 
-router.post("/signup", auth, password, userCtrl.signup);
-// router.post("/login", auth, userCtrl.login);
+router.post("/signup", password, userCtrl.signup);
+router.post("/signin", userCtrl.signin);
+router.post("/signout", userCtrl.signout);
 
 module.exports = router;
