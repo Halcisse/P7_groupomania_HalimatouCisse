aa8a7a8c49e1d67a5432bf7fb753e10efdb927ba
Halimatou cisse <halimatou.cisse@gmail.com>
2022-07-05 02:30:01 +0200

    router admin public

--- frontend/GROUPOMANIA_VUEJS/src/router/index.js
+++ frontend/GROUPOMANIA_VUEJS/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(""),
   routes: [
+    // Routage Public
     {
       path: "/",
       name: "public",
@@ -26,6 +27,39 @@ const router = createRouter({
       ],
     },
 
+    // Routage Admin
+    {
+      path: "/admin",
+      name: "admin",
+      component: () => import("../views/admin/LayoutAdmin.vue"),
+      children: [
+        {
+          path: "dashboard",
+          name: "dashboard",
+          component: () => import("../views/admin/Dashboard.vue"),
+        },
+        {
+          path: "users/index",
+          component: () => import("../views/admin/users/UserIndex.vue"),
+        },
+        {
+          path: "users/edit/:id",
+          component: () => import("../views/admin/users/UserEdit.vue"),
+        },
+        {
+          path: "users/add",
+          component: () => import("../views/admin/users/UserAdd.vue"),
+        },
+        {
+          path: "posts/index",
+          component: () => import("../views/admin/posts/PostsIndex.vue"),
+        },
+        {
+          path: "posts/edit/:id",
+          component: () => import("../views/admin/posts/PostsEdit.vue"),
+        },
+      ],
+    },
     // redirection vers 404 en cas de mauvais routage
     {
       path: "/:PathMatch(.*)*",
