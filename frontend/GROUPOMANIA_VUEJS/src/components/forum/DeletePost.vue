086f6048cc75a5ad9cf564db8337165580e0247f
Halimatou cisse <halimatou.cisse@gmail.com>
2022-08-16 17:36:24 +0200

    delete avec post affiché, ok

--- frontend/GROUPOMANIA_VUEJS/src/components/forum/DeletePost.vue
+++ frontend/GROUPOMANIA_VUEJS/src/components/forum/DeletePost.vue
@@ -4,7 +4,7 @@
       <h1>Êtes-vous sûr?</h1>
       <p>Toute suppression est définitive!</p>
     </div>
-    <div class="post">{{ this.post }}</div>
+    <div class="post">{{ post.message }} {{ post.imageUrl }}</div>
     <div class="btn">
       <button @click="deleteOne">SUPPRIMER LE POST</button>
       <router-link to="/forum">ANNULER</router-link>
@@ -15,16 +15,33 @@
 <script>
 export default {
   name: "delete",
-
-  data: () => {
+  data() {
     return {
-      post: "",
+      post: {
+        message: "",
+        imageUrl: null,
+      },
     };
   },
+  mounted() {
+    // alert("mounted");
+    let postId = this.$route.params.id;
+    let token = sessionStorage.getItem("token");
+    console.log(postId);
+
+    fetch(`http://localhost:3000/api/posts/${postId}`, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.post = data;
+      })
+      .catch((err) => console.log("err"));
+  },
   methods: {
-    getOne() {
-      //afficher le poste a supprimer
-    },
     deleteOne() {
       let postId = this.$route.params.id;
       let token = sessionStorage.getItem("token");
@@ -68,5 +85,17 @@ export default {
   justify-content: space-between;
   width: 500px;
   height: 100%;
+}
+
+.post {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f4f4f9;
+  margin: 15px 10px 44px 10px;
+  border-style: inset;
+  border-radius: 11px;
+  height: 100px;
+  width: 450px;
 }
 </style>
